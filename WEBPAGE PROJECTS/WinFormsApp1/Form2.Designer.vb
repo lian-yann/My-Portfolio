c3d8710b37669d6d2e94b0d3528b818816cf838b
Julian Nathaniel Cagadas <juliannathanielcagadas@gmail.com>
2025-07-16 00:34:13 +0800

    Add files via upload

---
+++ WEBPAGE PROJECTS/WinFormsApp1/Form2.Designer.vb
@@ -0,0 +1,396 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        Button4 = New Button()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        PictureBox10 = New PictureBox()
+        PictureBox11 = New PictureBox()
+        PictureBox12 = New PictureBox()
+        PictureBox13 = New PictureBox()
+        PictureBox14 = New PictureBox()
+        PictureBox15 = New PictureBox()
+        PictureBox16 = New PictureBox()
+        PictureBox17 = New PictureBox()
+        PictureBox18 = New PictureBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox14, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox15, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox16, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox17, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox18, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox1.BorderStyle = BorderStyle.Fixed3D
+        PictureBox1.Cursor = Cursors.No
+        PictureBox1.Location = New Point(15, 92)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(752, 323)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), Image)
+        PictureBox2.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox2.BorderStyle = BorderStyle.Fixed3D
+        PictureBox2.Location = New Point(81, 165)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(73, 112)
+        PictureBox2.TabIndex = 2
+        PictureBox2.TabStop = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        Button4.BackgroundImageLayout = ImageLayout.Stretch
+        Button4.FlatStyle = FlatStyle.Popup
+        Button4.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        Button4.Location = New Point(34, 31)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(159, 34)
+        Button4.TabIndex = 9
+        Button4.Text = "HOME"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), Image)
+        PictureBox3.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox3.BorderStyle = BorderStyle.Fixed3D
+        PictureBox3.Location = New Point(198, 165)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(91, 112)
+        PictureBox3.TabIndex = 10
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), Image)
+        PictureBox4.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox4.BorderStyle = BorderStyle.Fixed3D
+        PictureBox4.Location = New Point(330, 165)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(100, 112)
+        PictureBox4.TabIndex = 11
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), Image)
+        PictureBox5.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox5.BorderStyle = BorderStyle.Fixed3D
+        PictureBox5.Location = New Point(473, 165)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(91, 112)
+        PictureBox5.TabIndex = 12
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), Image)
+        PictureBox6.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox6.BorderStyle = BorderStyle.Fixed3D
+        PictureBox6.Location = New Point(608, 165)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(108, 112)
+        PictureBox6.TabIndex = 13
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox7.BackgroundImage = My.Resources.Resources._1000038385
+        PictureBox7.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox7.Location = New Point(733, 367)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(24, 33)
+        PictureBox7.TabIndex = 14
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox8.BackgroundImage = CType(resources.GetObject("PictureBox8.BackgroundImage"), Image)
+        PictureBox8.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox8.Location = New Point(50, 117)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(83, 29)
+        PictureBox8.TabIndex = 15
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox9.BackgroundImage = CType(resources.GetObject("PictureBox9.BackgroundImage"), Image)
+        PictureBox9.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox9.Location = New Point(50, 322)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(126, 30)
+        PictureBox9.TabIndex = 16
+        PictureBox9.TabStop = False
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox10.BackgroundImage = CType(resources.GetObject("PictureBox10.BackgroundImage"), Image)
+        PictureBox10.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox10.Location = New Point(168, 322)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(137, 30)
+        PictureBox10.TabIndex = 17
+        PictureBox10.TabStop = False
+        ' 
+        ' PictureBox11
+        ' 
+        PictureBox11.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox11.BackgroundImage = CType(resources.GetObject("PictureBox11.BackgroundImage"), Image)
+        PictureBox11.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox11.Location = New Point(311, 322)
+        PictureBox11.Name = "PictureBox11"
+        PictureBox11.Size = New Size(137, 30)
+        PictureBox11.TabIndex = 18
+        PictureBox11.TabStop = False
+        ' 
+        ' PictureBox12
+        ' 
+        PictureBox12.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox12.BackgroundImage = CType(resources.GetObject("PictureBox12.BackgroundImage"), Image)
+        PictureBox12.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox12.Location = New Point(445, 322)
+        PictureBox12.Name = "PictureBox12"
+        PictureBox12.Size = New Size(137, 30)
+        PictureBox12.TabIndex = 19
+        PictureBox12.TabStop = False
+        ' 
+        ' PictureBox13
+        ' 
+        PictureBox13.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox13.BackgroundImage = CType(resources.GetObject("PictureBox13.BackgroundImage"), Image)
+        PictureBox13.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox13.Location = New Point(588, 322)
+        PictureBox13.Name = "PictureBox13"
+        PictureBox13.Size = New Size(137, 30)
+        PictureBox13.TabIndex = 20
+        PictureBox13.TabStop = False
+        ' 
+        ' PictureBox14
+        ' 
+        PictureBox14.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox14.BackgroundImage = CType(resources.GetObject("PictureBox14.BackgroundImage"), Image)
+        PictureBox14.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox14.Location = New Point(76, 283)
+        PictureBox14.Name = "PictureBox14"
+        PictureBox14.Size = New Size(78, 33)
+        PictureBox14.TabIndex = 21
+        PictureBox14.TabStop = False
+        ' 
+        ' PictureBox15
+        ' 
+        PictureBox15.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox15.BackgroundImage = CType(resources.GetObject("PictureBox15.BackgroundImage"), Image)
+        PictureBox15.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox15.Location = New Point(624, 283)
+        PictureBox15.Name = "PictureBox15"
+        PictureBox15.Size = New Size(81, 33)
+        PictureBox15.TabIndex = 22
+        PictureBox15.TabStop = False
+        ' 
+        ' PictureBox16
+        ' 
+        PictureBox16.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox16.BackgroundImage = CType(resources.GetObject("PictureBox16.BackgroundImage"), Image)
+        PictureBox16.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox16.Location = New Point(179, 286)
+        PictureBox16.Name = "PictureBox16"
+        PictureBox16.Size = New Size(126, 30)
+        PictureBox16.TabIndex = 23
+        PictureBox16.TabStop = False
+        ' 
+        ' PictureBox17
+        ' 
+        PictureBox17.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox17.BackgroundImage = CType(resources.GetObject("PictureBox17.BackgroundImage"), Image)
+        PictureBox17.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox17.Location = New Point(311, 283)
+        PictureBox17.Name = "PictureBox17"
+        PictureBox17.Size = New Size(137, 33)
+        PictureBox17.TabIndex = 24
+        PictureBox17.TabStop = False
+        ' 
+        ' PictureBox18
+        ' 
+        PictureBox18.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        PictureBox18.BackgroundImage = CType(resources.GetObject("PictureBox18.BackgroundImage"), Image)
+        PictureBox18.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox18.Location = New Point(459, 280)
+        PictureBox18.Name = "PictureBox18"
+        PictureBox18.Size = New Size(123, 36)
+        PictureBox18.TabIndex = 25
+        PictureBox18.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        Button1.BackgroundImageLayout = ImageLayout.Stretch
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        Button1.Location = New Point(405, 31)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(159, 34)
+        Button1.TabIndex = 26
+        Button1.Text = "EVENTS"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        Button2.BackgroundImageLayout = ImageLayout.Stretch
+        Button2.FlatStyle = FlatStyle.Popup
+        Button2.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        Button2.Location = New Point(219, 31)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(159, 34)
+        Button2.TabIndex = 27
+        Button2.Text = "REGULAR"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        Button3.BackgroundImageLayout = ImageLayout.Stretch
+        Button3.FlatStyle = FlatStyle.Popup
+        Button3.Font = New Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        Button3.Location = New Point(588, 31)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(159, 34)
+        Button3.TabIndex = 28
+        Button3.Text = "ABOUT"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(781, 484)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox18)
+        Controls.Add(PictureBox17)
+        Controls.Add(PictureBox16)
+        Controls.Add(PictureBox15)
+        Controls.Add(PictureBox14)
+        Controls.Add(PictureBox13)
+        Controls.Add(PictureBox12)
+        Controls.Add(PictureBox11)
+        Controls.Add(PictureBox10)
+        Controls.Add(PictureBox9)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox3)
+        Controls.Add(Button4)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form2"
+        Text = "Form2"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox14, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox15, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox16, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox17, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox18, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents PictureBox14 As PictureBox
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents PictureBox16 As PictureBox
+    Friend WithEvents PictureBox17 As PictureBox
+    Friend WithEvents PictureBox18 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
